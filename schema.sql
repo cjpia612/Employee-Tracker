a933cbcd67d78a2349a6e49735b35ba27296009c
Cattleya Pia <cjpia612@gmail.com>
2020-06-27 16:47:27 -0700

    created tables in schema moved from originally being in seed file

--- schema.sql
+++ schema.sql
@@ -13,16 +13,18 @@ CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,4) NULL,
-  department_id INT default 0,
-  highest_bid INT default 0,
-  PRIMARY KEY (id)
+  department_id INT NOT NULL,
+  PRIMARY KEY (id), 
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT default 0,
-  manager_id INT default 0,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
   PRIMARY KEY (id)
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
